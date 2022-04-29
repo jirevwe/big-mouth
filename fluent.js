15c2cf0f169610dc525f9c154f530258acc40350
bossman-codex <56489079+bossman-codex@users.noreply.github.com>
2022-04-29 07:35:23 +0100

    added the fluent-ffmpeg

---
+++ fluent.js
@@ -0,0 +1,23 @@
+const ffmpeg = require('fluent-ffmpeg');
+
+const firstFile = "aud-0.mp3";
+const secondFile = "aud-1.mp3";
+const thirdFile = "aud-2.mp3";
+const fourthFile = "aud-3.mp3";
+const outPath = "out.mp3";
+
+function merge(){
+     ffmpeg(firstFile)
+    .input(secondFile)
+    .input(thirdFile)
+    .input(fourthFile)
+    .on('end', function() {
+      console.log('files have been merged succesfully');
+    })
+    .on('error', function(err) {
+      console.log('an error happened: ' + err.message);
+    })
+    .mergeToFile(outPath);
+} 
+
+module.exports = merge;
--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const fs = require("fs");
 const googleTTS = require("google-tts-api");
 const https = require("https");
 const shellExec = require("./exec");
+const merge = require("./fluent");
 
 const run = async () => {
-  const text = `We want Convoy to run without any dependencies. Today when you install loki you don't connect it to a db. Meaning we want to own all the tech even if it's embedded. This might mean we'd switch our main storage from mongodb to something smaller like SQLite and having an robust disk db solution. We'd also want to not depend on Redis or only depend on Redis for both storage and queue. This decision has helped to separate critical parts of code to different packages and we’ve seen increased reliability in our production instance. In the past we used to scale up both server and worker, now we can scale up server and worker workloads independently.`;
+  const text = `We want Convoy to run without any dependencies. Today when you install loki you don't connect it to a db. Meaning we want to own all the tech even if it's embedded. This might mean we'd switch our main storage from mongodb to something smaller like SQLite and having an robust disk db solution. We'd also want to not depend on Redis or only depend on Redis for both storage and queue. This decision has helped to separate critical parts of code to different packages and we’ve seen increased reliability in our production instance. In the past we used to scale up both server and worker, now we can scale up server and worker workloads independently. Now i have added some files i want to see if it can merge sucessfully and reach one thousand characters with out stopping or breaking , so i dont look like a useless developer`;
   console.log(text.length);
 
   // get base64 text`
@@ -21,10 +22,10 @@ const run = async () => {
     console.log(res);
   }
 
-  const cmd = `ffmpeg -i aud-0.mp3 -i aud-1.mp3  -i aud-2.mp3 -i aud-3.mp3 \
-  -filter_complex '[0:0][1:0][2:0][3:0]concat=n=4:v=0:a=1[out]' -map '[out]' aud.mp3`;
+  const cmd = `ffmpeg -i aud-0.mp3 -i aud-1.mp3 -i aud-2.mp3 -i aud-3.mp3`
 
   const out = await shellExec(cmd);
+  await merge()
   console.log(out);
 };
 
