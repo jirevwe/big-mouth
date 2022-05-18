8fc1af2b3ac433e6d09831d77abb50b5972792bb
bossman-codex <56489079+bossman-codex@users.noreply.github.com>
2022-05-18 15:36:46 +0100

    updated the cli section commands

--- command.js
+++ command.js
@@ -9,57 +9,32 @@ const optionDefinitions = [
 
 const sections = [
   {
-    header: "Rover Movement Sequence v0.1.0",
+    header: "Text-Audio Converter",
     content: `Usage:
-    yarn start --args
+    npm start --args
     
     Examples:
-    yarn start -x 0 -y 0 -d NORTH -i LBRF
-    yarn start -x 4 -y 2 -d EAST -i FLFFFRFLB
-    yarn start --x-position 4 --y-position 2 --direction EAST --input FLFFFRFLB
+    yarn start -u=url -o=pathname.mp3 
+    yarn start --url=websiteurl --output=pathname.mp3
     `,
   },
   {
     header: "Options",
     optionList: [
       {
-        name: "x-position",
-        alias: "x",
+        name: "url",
+        alias: "u",
         type: String,
         description:
-          "required, the x coordinate where the rover will be initialised",
+          "required, Website url of blogPost",
       },
       {
-        name: "y-position",
-        alias: "y",
+        name: "output",
+        alias: "o",
         type: String,
         description:
-          "required, the y coordinate where the rover will be initialised",
-      },
-      {
-        name: "input",
-        alias: "i",
-        type: String,
-        description: `required, the input that the rover is will process.
-        One of L, R, F, B
-
-        F: Move the rover forward with the current heading
-        B: Move the rover backward with the current heading
-        L: Rotate the rover left by 90 degrees
-        R: Rotate the rover right by 90 degrees`,
-      },
-      {
-        name: "direction",
-        alias: "d",
-        description: `required, the direction where the rover will face when initialised.
-        One of NORTH, EAST, WEST, SOUTH`,
-      },
-      {
-        name: "help",
-        alias: "h",
-        type: Boolean,
-        description: "Print this usage guide.",
-      },
+          "required, the name of the merged audio file ,example (output.mp3)",
+      },  
     ],
   },
 ];
