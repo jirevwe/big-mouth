e80dec69c605597fb2fb1ca33ebec27fcad6c86c
Emmanuel Tukpe <tukpeemmanuel@gmail.com>
2022-05-30 14:25:11 +0100

    feat: fixed bug issue regarding readabilty

--- html.js
+++ html.js
@@ -1,17 +1,19 @@
-const request = require("request");
-const cheerio = require("cheerio");
+//const request = require("request");
+//const cheerio = require("cheerio");
+const {Readability} = require("@mozilla/readability")
+const { JSDOM } = require("jsdom")
+const axios = require("axios")
 
-function parseHTMLPage(url) {
-  return new Promise((resolve, reject) => {
-    request(url, function (error, response, html) {
-      if (!error && response.statusCode == 200) {
-        const raw = cheerio.load(html);
-        return resolve(raw.text());
-      } else {
-        return reject(error);
-      }
-    });
-  });
+async function parseHTMLPage(url) {
+  const { data } = await axios.get(url, {timeout: this.timeout})
+  const doc = new JSDOM(data)
+  let reader = new Readability(doc.window.document)
+  let article = reader.parse()
+
+  console.log(article.textContent);
+
+  return article.textContent
 }
+
 
 exports.parseHTMLPage = parseHTMLPage;
