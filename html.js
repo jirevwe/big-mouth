c3f73bbb5b13cc31dc363f9aa4e803a4e79e230d
Emmanuel Tukpe <tukpeemmanuel@gmail.com>
2022-05-30 14:42:52 +0100

    chore: removed unimportant code

--- html.js
+++ html.js
@@ -1,17 +1,13 @@
-const {Readability} = require("@mozilla/readability")
-const { JSDOM } = require("jsdom")
-const axios = require("axios")
+const { Readability } = require("@mozilla/readability");
+const { JSDOM } = require("jsdom");
+const axios = require("axios");
 
 async function parseHTMLPage(url) {
-  const { data } = await axios.get(url, {timeout: this.timeout})
-  const doc = new JSDOM(data)
-  let reader = new Readability(doc.window.document)
-  let article = reader.parse()
-
-  console.log(article.textContent);
-
-  return article.textContent
+  const { data } = await axios.get(url, { timeout: this.timeout });
+  const doc = new JSDOM(data);
+  let reader = new Readability(doc.window.document);
+  let article = reader.parse();
+  return article.textContent;
 }
-
 
 exports.parseHTMLPage = parseHTMLPage;
